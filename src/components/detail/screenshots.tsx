e9c3977a0587b013d895a30da6490058ad9a2914
dylangavilan <dylan.gavilan32@gmail.com>
2024-11-15 13:17:54 -0300

    fix short carousel

--- src/components/detail/screenshots.tsx
+++ src/components/detail/screenshots.tsx
@@ -26,29 +26,42 @@ const Screenshots = ({ items }: Props) => {
           items: items.length < 4 ? items.length : 4
         }
       };
+
+      const ShowItems = () => {
+       return  ( 
+        items.map((screenshot: Screenshots) => {
+          const coverUrl = getCover('cover_big', screenshot.image_id);
+          return (
+            <div
+              key={screenshot.image_id}
+              className="relative w-[83.5px] h-[83.5px] md:w-[170px] md:h-[160px]"
+            >
+              <Image
+                src={coverUrl}
+                alt={`Screenshot ${screenshot.image_id}`}
+                className="rounded-lg"
+                fill
+                sizes="(max-width: 640px) 83.5px, 170px"
+              />
+            </div>
+          );
+        })
+      )
+      }
+ 
+      if(items.length < 4){
+        return (
+        <div className='flex gap-4'>
+          <ShowItems />
+        </div>
+     )}
+      
       return ( 
-      <Carousel 
-      itemClass="carousel-item-padding-40-px"
-      infinite
-      responsive={responsive}>
-          {items.map((screenshot: Screenshots) => {
-            const coverUrl = getCover('cover_big', screenshot.image_id);
-            return (
-              <div
-                key={screenshot.image_id}
-                className="relative w-[83.5px] h-[83.5px] md:w-[170px] md:h-[160px]"
-              >
-                <Image
-                  src={coverUrl}
-                  alt={`Screenshot ${screenshot.image_id}`}
-                  className="rounded-lg"
-                  fill
-                  sizes="(max-width: 640px) 83.5px, 170px"
-                />
-              </div>
-            );
-          })}
-      </Carousel>
+        <Carousel itemClass="carousel-item-padding-40-px"
+                  infinite
+                  responsive={responsive}>
+            <ShowItems />
+        </Carousel>
       )
     }
     export default Screenshots
