b7461c03e7d1acb684aecd91c3ced423f0e70707
dylangavilan <dylan.gavilan32@gmail.com>
2024-11-15 15:06:20 -0300

    add OG page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,10 @@ const inter = Inter({
 export const metadata: Metadata = {
   title: "Gaming Having Z",
   description: "The place where you will can choose your games",
+  openGraph: {
+    title: "Gaming Having Z",
+    description: "All you want in one place, search what will be your next adventure"
+  },
 };
 
 export default function RootLayout({
